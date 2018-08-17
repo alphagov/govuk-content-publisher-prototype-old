441b14cf4dd6ad9dd6389256bd37d4a92c5fca8b
sonia.turcotte@gmail.com <sonia.turcotte@gmail.com>
2018-08-17 13:42:07 +0100

    Add branching for publish flow

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Branching
+router.post('/confirm-publish', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let publish = req.session.data['publish']
+
+  if (publish === 'Publish') {
+    res.redirect('/confirm-publish')
+  } else if (publish === 'Force publish') {
+    res.redirect('/confirm-force-published')
+  } else {
+    res.redirect('/submitted')
+  }
+})
+
 module.exports = router
