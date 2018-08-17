75727af0cbfb120179f4c41fd093b45a067a2893
sonia.turcotte@gmail.com <sonia.turcotte@gmail.com>
2018-08-17 13:41:50 +0100

    Add routing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,19 +9,3 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-
-// Branching publishing options
-
-router.post('/publish', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  let publish = req.session.data['publish']
-
-  if (publish === 'false') {
-    res.redirect('confirm-force-publish')
-  } else {
-    res.redirect('confirm-force-published')
-  }
-})
