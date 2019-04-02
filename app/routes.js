df3cf9d434f26cd5d4c8aea7beb3328e8113f287
SoniaTurcotte <sonia.turcotte@gmail.com>
2019-04-02 09:42:49 +0100

    add branching for schedule options

--- app/routes.js
+++ app/routes.js
@@ -21,3 +21,20 @@ router.post('/confirm-publish', function (req, res) {
 })
 
 module.exports = router
+
+
+
+// Branching
+router.post('/schedule', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let schedule = req.session.data['save']
+
+  if (schedule === 'save') {
+    res.redirect('/content-summary-submitted-to-schedule-confirm')
+  } else {
+    res.redirect('/schedule-2')
+  }
+})
